e5a3f6c62ecd55cdb919b417863d09879a1b5e07
Arina M <apelsinka7.ru@yandex.ru>
2022-09-20 21:36:03 +0300

    решение задачи4

---
+++ Zadacha4_ArinaM/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 3 числа");
+int a, b, d, max;
+Console.WriteLine("Введите первое число - "); 
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число - ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число - ");
+d = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+ 
+if (b>max)
+max = b;
+
+if (d>max)
+max = d;
+
+Console.WriteLine($"Максимальное число - {max}");
